3f9429a66b39df6c92f0a237699e0dd8fc9bbec1
k3mp314 <k3mp314@gmail.com>
2025-09-02 11:40:23 +0000

    [Add] router to save messages

--- Lektion-01/server/app.mjs
+++ Lektion-01/server/app.mjs
@@ -1,15 +1,38 @@
 import express from "express"
 import cors from "cors"
+import fs from "fs"
+import { fileURLToPath } from "url"
 
 const app = express()
-
-const port = 3000
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-})
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
+
+const saveMessage = () => {
+ fs.writeFileSync(fileURLToPath, JSON.stringify(message))
+}
+
+app.post("/messages", (req, res) => {
+    const {name, message} = req.body
+
+    try {
+        const newMessage = {
+            name,
+            message,
+            timestamp: new Date().toISOString()
+        }
+    
+        saveMessage(message)
+
+        res.status(201).json("Saved successfully")
+    } catch (error) {
+        console.log("Error: ", error)
+        res.status(500).json("Inernal server error");
+    }
+    
+
+
+})
 
 export default app
