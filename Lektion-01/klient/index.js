20e0d98cbd7a24de31a8c89c5ea0c8fe3cc32394
k3mp314 <k3mp314@gmail.com>
2025-09-09 11:58:30 +0000

    [Add] find and update JSON file content

--- Lektion-01/klient/index.js
+++ Lektion-01/klient/index.js
@@ -1,10 +1,10 @@
-
-
 const form = document.querySelector(".input-form") // Läs in formulär
 const submitBtn = document.querySelector("button[type='submit']") // Läs in submit knapp
 
 let inputName = form.elements.name.value // Läser in det som skrivs i namn input 
 let inputMessage = form.elements.message.value //Läser in det som skrivs i message input
+
+const backendUrl = "https://3000-firebase-wesweb01-1756207605389.cluster-ha3ykp7smfgsutjta5qfx7ssnm.cloudworkstations.dev/messages"
 
 const checkInputs = () => {
     inputName = form.elements.name.value
@@ -20,5 +20,28 @@ form.addEventListener("submit", async (e) => { // e förkortning för event
     e.preventDefault() // Hindrar formuläret från att ladda om
 
     if (!inputName || !inputMessage) return alert("Fyll i båda fälten!")
+
+        try {
+            const response = await fetch(backendUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    name: inputName,
+                    message: inputMessage
+                })  
+            })
+
+            if (response.ok) {
+                alert("Meddelandet sparades!")
+                form.reset()
+            } else {
+                alert("Ett fel uppstod!")
+            }
+        } catch (error) {
+            console.error("Fel:", error)
+            alert("Kunde inte skicka meddelandet")
+        } 
 })
 
--- Lektion-01/server/app.mjs
+++ Lektion-01/server/app.mjs
@@ -1,29 +1,46 @@
 import express from "express"
-import cors from "cors"
-import fs from "fs"
-import { fileURLToPath } from "url"
+import cors from "cors" // Tillåter förfrågningar fån andra domäner (Cross-Origin Recource Sharing)
+import fs from "fs" // Node.js filsystem-modul för att läsa och skriva filer
+import { fileURLToPath } from "url" // Hjälper oss att få sökvägen till den aktuella filen
+import { dirname } from "path" // Hjälper oss att få sökvägen till den aktuella mappen
 
-const app = express()
+const __filename = fileURLToPath(import.meta.url) // Hjälper oss att få sökvägen till den aktuella filen
+const __dirname = dirname(__filename) // Hjälper oss att få sökvägen till den aktuella mappen
+
+const app = express() // Skapa Express-applikationen
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 
-const saveMessage = () => {
- fs.writeFileSync(fileURLToPath, JSON.stringify(message))
+const saveMessage = (messageData) => {
+ const filePath = `${__dirname}/messages.json` // Skapa fullständign sökväg till JSON-fil
+
+ let messages = []
+ if (fs.existsSync(filePath)) { // Kontrollera om filen faktiskt finns
+    const data = fs.readFileSync(filePath, "utf-8") // Läs fil som text
+    messages = JSON.parse(data) // Konvertera JSON-text till JavaScript array
+ }
+
+ messages.push(messageData) // Lägg till det nya meddelande-objektet sist i arrayen
+
+ // Spara tillbaka hela arrayen till filen
+ // JSON.stringify() konverterar JS till JSON-text
+ // null, 2 gör JSON-filen lättläst med indentering
+ fs.writeFileSync(filePath, JSON.stringify(messages, null, 2))
 }
 
 app.post("/messages", (req, res) => {
     const {name, message} = req.body
 
     try {
-        const newMessage = {
+        const messageData = {
             name,
             message,
             timestamp: new Date().toISOString()
         }
     
-        saveMessage(message)
+        saveMessage(messageData)
 
         res.status(201).json("Saved successfully")
     } catch (error) {
